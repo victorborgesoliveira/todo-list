c8032d4442975003423c907b5d1fb7c636fed172
Victor Borges <vituze1@gmail.com>
2026-01-03 16:34:45 -0300

    setupDB

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime, UTC
 
 def main():
+    setupDB()
     createTask()
 
 def createTask():
@@ -8,4 +9,14 @@ def createTask():
     status = 'created'
     created_at = datetime.now(UTC)
     print('new task added: ' + description + ', with status: ' + status)
+
+def setupDB():
+    filename = 'db.json'
+    try:
+        with open(filename): 
+            print('Database already created')
+    except FileNotFoundError:
+        with open(filename, "x"):
+            print('Database created')
+
 main() 
